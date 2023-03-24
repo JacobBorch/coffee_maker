a9c58e9b8233386cb9b7881d8f1d628628287aae
supersanta183 <au583665@post.au.dk>
2023-03-24 20:49:41 +0100

    .

--- src/coffee_machine.rs
+++ src/coffee_machine.rs
@@ -1,16 +1,27 @@
+use time::Time;
+
+const COFFE_BREW_TIME: usize = 5; //time in minutes
+
 pub struct CoffeeMachine {
-    timer: usize
+    timer: usize,
+    pub is_available: bool,
 }
 
 impl CoffeeMachine {
 
     pub fn new() -> Self {
         CoffeeMachine {
-            timer: 0
+            timer: 0,
+            is_available: true,
         }
     }
 
-    pub fn brew() {
+    pub fn tick(&mut self) -> {
+
+    }
+
+    pub fn brew(&self) {
+        timer = COFFE_BREW_TIME;
         todo!()
     }
 }
